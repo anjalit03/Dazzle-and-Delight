316d6b77b5d6cd6570de58d7df066098adb4985e
AananditKanwar <144915308+AananditKanwar@users.noreply.github.com>
2024-10-30 16:47:40 +0530

    Updates to the contacts page UI

---
+++ html/js/contact_script.js
@@ -0,0 +1,18 @@
+const inputs = document.querySelectorAll(".contact-input");
+inputs.forEach( ipt =>{
+    ipt.addEventListener("focus", () => {
+        ipt.parentNode.classList.add("focus");
+        ipt.parentNode.classList.add("not-empty");
+    });
+    ipt.addEventListener("blur", () => {
+        ipt.parentNode.classList.remove("focus");
+    });
+});
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    });
+});
